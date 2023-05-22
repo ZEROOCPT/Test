184eb58a97b012e86f0785453f13b668fcbb313f
ZEROOCPT <105050106+ZEROOCPT@users.noreply.github.com>
2023-05-22 18:31:42 +0800

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,17 @@
+
+meme_dict = {
+            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
+            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
+            "ROLF":"tanggapan terhadap lelucon",
+            "SHEESH":"sedikit ketidaksetujuan",
+            "AGGRO":"untuk menjadi agresif/marah",
+            }
+for i in range (3):       
+    word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+    if word in meme_dict.keys():
+        # Apa yang harus kita lakukan jika kata itu ditemukan?
+        print(meme_dict[word])
+        
+    else:
+        # Apa yang harus kita lakukan jika kata itu tidak ditemukan?
+        print("1")
